e79e0ebb136b68c184f703ef2d51203838ef0f2d
kazuCF <kk1719365@gmail.com>
2016-07-02 14:10:20 +0900

    1000*1000

--- ReadCsv/GeoData.cs
+++ ReadCsv/GeoData.cs
@@ -11,8 +11,8 @@ namespace ReadCsv
         public  string name;
         public  string[] columns;
         public  List<string[]> datas;
-        //  public Way way = new Way(4000, 4000,new double[] { 99.49875,103.8643},new double[] {12.18,16.10 });
-        public Way way = new Way(1000, 1000, new double[] { 100.5, 100.8 }, new double[] { 13.68, 13.80 });
+          public Way way = new Way(1000, 1000,new double[] { 99.49875,103.8643},new double[] {12.18,16.10 });
+//public Way way = new Way(1000, 1000, new double[] { 100.5, 100.8 }, new double[] { 13.68, 13.80 });
         public GeoData(string Name,string[]Columns, List<string[]> Datas)
         {
             name = Name;  columns = Columns;datas = Datas;
